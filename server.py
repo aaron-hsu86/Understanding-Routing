7bba1079067ed931b76cc98dfa41ed386eff23d6
Aaron <aaron.hsu86+github@gmail.com>
2023-06-08 11:52:23 -0700

    first commit

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/dojo')
+def dojo():
+    return 'dojo'
+
+@app.route('/say/<name>')
+def hello_name(name):
+    if name.isnumeric() == True:
+        return "Please don't use numbers"
+    else:
+        return f'Hi {name}!'
+
+@app.route('/repeat/<num>/<result>')
+def repeat_result_num(num, result):
+    if num.isnumeric():
+        return f'<p>{result}</p>'*int(num)
+    else:
+        return f'{num} is not a valid number. Please provide a valid number.'
+    
+@app.route('/<other>')
+def other_pages(other):
+    return 'Sorry! No response. Try again.'
+    
+
+# app.run(debug=True) should be the very last statement! 
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
